7a42c972e55d0a3c77847dc28de0345490463ae4
currocam <campuzanocurro@gmail.com>
2022-10-27 13:41:37 +0000

    fixed: empty filtered fasta

--- workflow/scripts/cp_metarib.py
+++ workflow/scripts/cp_metarib.py
@@ -3,4 +3,4 @@ infiles = snakemake.input.R1 + snakemake.input.R2
 outfiles = snakemake.output.R1 + snakemake.output.R2
 threads = snakemake.threads
 for old, new in zip(infiles, outfiles):
-    os.system(f"pigz -dk -p{threads} {old} > {new}")
+    os.system(f"pigz -dkf -p{threads} < {old} > {new}")
