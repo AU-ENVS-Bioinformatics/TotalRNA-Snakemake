0b5b19f89ff9dffe932da6f5e80554161644a54e
Curro Campuzano <campuzanocurro@gmail.com>
2023-09-12 15:42:11 +0000

    fix: non coding rna

--- workflow/scripts/processing_cmsearch_tbl.py
+++ workflow/scripts/processing_cmsearch_tbl.py
@@ -1,57 +1,43 @@
-from pathlib import Path
-import sys
 import pandas as pd
+import sys
+
+
+def parse_cmsearch_line(line):
+    line = line.split()
+    return pd.Series(
+        {
+            "target_name": line[0],
+            "query_name": line[2],
+            "accession": line[3],
+            "score": float(line[14]),
+            "evalue": float(line[15]),
+        }
+    )
+
 
 infile = snakemake.input[0]
 outfile = snakemake.output[0]
 evalue = snakemake.params.evalue
 
-column_names = [
-    "target_name",
-    "accession",
-    "query_name",
-    "accession2",
-    "mdl",
-    "mdl_from",
-    "mdl_to",
-    "seq_from",
-    "seq_to",
-    "strand",
-    "trunc",
-    "pass",
-    "gc",
-    "bias",
-    "score",
-    "E-value",
-    "inc",
-    "description_length",
-    "description_path",
-]
-
-
 with open(snakemake.log[0], "w") as f:
     sys.stderr = sys.stdout = f
     print(f"Reading file: {infile}", file=sys.stderr)
-    df = pd.read_csv(
-        infile,
-        delimiter="\s+",
-        header=None,
-        comment="#",
-        names=column_names,
+    ## Create dataframe from a list of dictionaries
+
+    df = pd.DataFrame(
+        [
+            parse_cmsearch_line(line)
+            for line in open(infile)
+            if not line.startswith("#")
+        ],
     )
-    # Join description_path column and description_length column into one
-    df["description"] = df["description_path"].str.cat(
-        df["description_length"].astype(str), sep=" "
-    )
-    # Drop description_path and description_length columns
-    df = df.drop(columns=["description_path", "description_length"])
     print(f"Shape before: {df.shape}", file=sys.stderr)
     # Filter by E-value
     print(f"Filtering by E-value <= {evalue}", file=sys.stderr)
-    df = df[df["E-value"] <= evalue]
+    df = df[df["evalue"] <= evalue]
     print(f"Shape before filtering by E-value: {df.shape}", file=sys.stderr)
     # Order by score and E-value
-    df = df.sort_values(by=["score", "E-value"], ascending=[False, True])
+    df = df.sort_values(by=["score", "evalue"], ascending=[False, True])
     # Remove duplicates of column "target_name"
     df = df.drop_duplicates(subset=["target_name"])
     print(f"Shape after removing duplicates: {df.shape}", file=sys.stderr)
