eb6b5f33bc654f5dcfd8aa20c471a8913ee4ae1e
Curro Campuzano <campuzanocurro@gmail.com>
2023-09-28 13:58:11 +0200

    Now you can skip renaming

--- workflow/rules/rename_input.smk
+++ workflow/rules/rename_input.smk
@@ -5,7 +5,16 @@ rule rename:
         "logs/renaming_files.log",
     conda:
         "../envs/base_python.yaml"
-    benchmark:
-        "benchmarks/renaming_files.log"
     script:
         "../scripts/rename_files.py"
+
+
+rule skip_rename:
+    output:
+        touch("results/rename.done"),
+    log:
+        "logs/renaming_files.log",
+    conda:
+        "../envs/base_python.yaml"
+    script:
+        "../scripts/skip_rename_files.py"
---
+++ workflow/scripts/skip_rename_files.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import re
+import sys
+
+DEFAULT_SOURCE_FILEPATH = "reads/"
+DEFAULT_DEST_FILEPATH = "results/"
+RENAMED_READS_FILEPATH = "renamed_raw_reads/"
+
+regular_expression = "*.fastq.gz"
+input_dir = DEFAULT_SOURCE_FILEPATH
+output_dir = f"{DEFAULT_DEST_FILEPATH}{RENAMED_READS_FILEPATH}"
+
+
+def copy_file(original: str, target: str) -> None:
+    src, dest = Path(original).absolute(), Path(target).absolute()
+    dest.parent.mkdir(parents=True, exist_ok=True)
+    print(src)
+    print(dest)
+    dest.symlink_to(src)
+
+
+with open(snakemake.log[0], "w") as f:
+    sys.stderr = sys.stdout = f
+    for file in os.listdir(input_dir):
+        match = re.match(regular_expression, file)
+        if match:
+            original = input_dir + match.string
+            target = output_dir + match.string
+            if os.path.exists(target):
+                print("The previous file was already there.", file=sys.stderr)
+            else:
+                copy_file(original, target)
