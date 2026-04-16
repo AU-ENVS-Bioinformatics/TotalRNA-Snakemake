a8bd90fd81d7f5a08492928b45a67bfca3a59009
helton <heltonciriaco@gmail.com>
2026-04-16 13:56:34 +0000

    fixed truncation

--- workflow/scripts/get_read_length.py
+++ workflow/scripts/get_read_length.py
@@ -40,7 +40,7 @@ with open(log[0], "w") as f:
 
     conn = duckdb.connect(database)
 
-    conn.execute("TRUNCATE TABLE IF EXISTS read_length")
+    conn.execute("TRUNCATE TABLE read_length")
 
     conn.execute("INSERT INTO read_length SELECT contig, sample, read_length FROM df WHERE read_length > 0")
 
